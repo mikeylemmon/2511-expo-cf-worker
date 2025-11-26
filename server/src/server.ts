54917c433d3d316b1c227855af5a61febb99382a
Mikey Lemmon <mikeylemmon@gmail.com>
2025-11-26 13:24:04 +1300

    Getting closer - app loads successfully
    But server functions aren't working...
    
       <<< fetch POST http://localhost:8787/_flight/web/ACTION_./actions/server-func.ts/serverFunc.txt
       [wrangler:error] TypeError: Illegal invocation: function called with incorrect `this` reference.
       See https://developers.cloudflare.com/workers/observability/errors/#illegal-invocation-errors for details.
           at null.<anonymous> (file:///.../src/dist/server/_expo/functions/_flight/%5B...rsc%5D.js:13:39)
           at f (file:///.../src/dist/server/_expo/functions/_flight/%5B...rsc%5D.js:2:2060)
           at a (file:///.../src/dist/server/_expo/functions/_flight/%5B...rsc%5D.js:2:1556)

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,30 +11,26 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-// import { createRequestHandler } from "expo-server/adapter/workerd";
-
-// interface AppEnv extends Env {
-// 	ASSETS: {
-// 		fetch: typeof fetch;
-// 	};
-// }
+import { createRequestHandler } from 'expo-server/adapter/workerd';
+// import path from 'node:path';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		console.log('<<< fetch', request.method, request.url);
 
-		// Seeing if we can import the routes.json file. This works...
-		const routesLiteral = await import('./dist/server/_expo/routes.json');
-		console.log('--- routes.json (literal path)', routesLiteral.default);
+		// // Seeing if we can import the routes.json file. This works...
+		// const routesLiteral = await import('./dist/server/_expo/routes.json');
+		// console.log('--- routes.json (literal path)', routesLiteral.default);
 
-		// ...but this doesn't...
-		try {
-			const routesPath = './dist/server/_expo/routes.json';
-			const routes = await import(routesPath, { with: { type: 'json' } }); // No such module "dist/server/_expo/routes.json"
-			console.log('--- routes.json (path variable)', routes.default);
-		} catch (error) {
-			console.log('--- routes.json error:', error);
-		}
+		// // ...but this doesn't...
+		// try {
+		// 	const routesPath = './dist/server/_expo/routes.json';
+		// 	const routes = await import(routesPath); // Returns a string, not an object
+		// 	// ^^^ If no rule in wrangler.jsonc, then: No such module "dist/server/_expo/routes.json"
+		// 	console.log('--- routes.json (path variable)', routes.default);
+		// } catch (error) {
+		// 	console.log('--- routes.json error:', error);
+		// }
 
 		// // ...and this doesn't either...
 		// try {
@@ -45,23 +41,16 @@ export default {
 		// 	console.log('--- routes.json error:', error);
 		// }
 
-		return new Response('Hello, World!', { status: 200 });
+		// return new Response('Hello, World!', { status: 200 });
 
-		// // const serverDir = path.join(process.cwd(), "server");
-		// // const serverDir = path.join(process.cwd(), "dist/server");
-		// const serverDir = "./dist/server";
-		// // const serverDir = "/dist/server";
-		// // const serverDir = "/server";
-		// console.log("--- serverDir", serverDir);
-		//
-		// const handler = createRequestHandler({
-		//   build: serverDir,
-		// });
-		// return await handler(request, env, ctx);
-		// // ^^^ Fails with:
-		// // [wrangler:error] TypeError: Cannot read properties of null (reading 'htmlRoutes')
-		// // at initManifestRegExp (file:///.../node_modules/expo-server/src/vendor/environment/common.ts:6:26)
-		// // at getRoutesManifest (file:///.../node_modules/expo-server/src/vendor/environment/common.ts:39:14)
-		// // at async handler (file:///.../node_modules/expo-server/src/vendor/abstract.ts:75:18)
+		const handler = createRequestHandler({
+			build: './dist/server',
+		});
+		return await handler(request, env, ctx);
+		// ^^^ Fails with:
+		// [wrangler:error] TypeError: Cannot read properties of null (reading 'htmlRoutes')
+		// at initManifestRegExp (file:///.../node_modules/expo-server/src/vendor/environment/common.ts:6:26)
+		// at getRoutesManifest (file:///.../node_modules/expo-server/src/vendor/environment/common.ts:39:14)
+		// at async handler (file:///.../node_modules/expo-server/src/vendor/abstract.ts:75:18)
 	},
 } satisfies ExportedHandler<Env>;
